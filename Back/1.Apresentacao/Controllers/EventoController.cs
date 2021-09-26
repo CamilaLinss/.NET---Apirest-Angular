1d68a7fd0c399d1a68f81a3bfb1f4d7434e1c64b
camizmiau <souzacamila215@ymail.com>
2021-09-26 15:28:36 -0300

    Adicionando entidade - mockando metodos

--- Back/1.Apresentacao/Controllers/EventoController.cs
+++ Back/1.Apresentacao/Controllers/EventoController.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using _2.Dominio.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _1.Apresentacao.Controllers
@@ -8,23 +11,81 @@ namespace _1.Apresentacao.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+
+
+        //MOCK INICIAL (FORMA BACANA DE MOCKAR OS DADOS INICIALMENTE)
+        private IEnumerable<Evento> listaEventos = new Evento[] {
+                
+                new Evento(){
+                    EventoId = 1,
+                    Local = "São Paulo, Osasco",
+                    Tema = "Empoderamento de mulheres !!",
+                    Lote = "1",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+                },
+                 new Evento(){
+                    EventoId = 2,
+                    Local = "Bahia, Salvador",
+                    Tema = "Empoderamento de mulheres !!",
+                    Lote = "5",
+                    QtdPessoas = 310,
+                    DataEvento = DateTime.Now.AddDays(10).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+                 }
+
+                };
+
+
+
         public EventoController()
         {
             
         }
 
 
-        [HttpGet]
-        public string Busca(){
 
-            return "Ola mundo";
+        [HttpGet]
+        public IEnumerable<Evento> Busca(){;
+
+            return listaEventos;
         }
         
-        
-        [HttpPost]
-        public string Cadastra(int id){
 
-            return $"Cadastrando registro";
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> BuscaId(int id){
+
+            //O where é aplicado em uma lista de items, onde consigo fazer uma especie de filtro/querie
+            //antes do recurso de fato
+
+            //eventox representa o objeto da lista que vai vir como resultado do where na lista 
+            //"listaEventos" (nome que coloquei para didatica)
+
+            //Dessa forma consigo simular a busca comqualquer id da lista
+            return listaEventos.Where(eventox => eventox.EventoId == id);
+        }
+        
+
+
+        [HttpPost]
+        public ActionResult Cadastra([FromBodyAttribute] Evento request){
+
+            Evento evento = new Evento(){
+
+                EventoId = request.EventoId,
+                Local = request.Local,
+                Tema = request.Tema,
+                Lote = request.Lote,
+                QtdPessoas = request.QtdPessoas,
+                DataEvento = DateTime.Now.AddDays(10).ToString("dd/MM/yyyy"),
+                //DataEvento = request.DataEvento,
+                ImagemURL = request.ImagemURL
+            };
+
+            listaEventos.ToList().Add(evento);
+
+            return NoContent();
 
         }
     
